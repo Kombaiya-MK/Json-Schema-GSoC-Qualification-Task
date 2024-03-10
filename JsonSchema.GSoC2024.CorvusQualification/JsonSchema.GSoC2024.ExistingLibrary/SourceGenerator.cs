39e63ba35601cdd25a0a82ba3197c46555fb91b0
Kombaiya Mariappan <kombaiya.kanini@gmail.com>
2024-03-11 01:01:27 +0530

    Source generator added

---
+++ JsonSchema.GSoC2024.CorvusQualification/JsonSchema.GSoC2024.ExistingLibrary/SourceGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonSchema.GSoC2024.ExistingLibrary
+{
+    [Generator]
+    public class SourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(ctx =>
+            {
+                var sourceText = @"using System;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+public class GeneratedAttributeAttribute : Attribute
+{
+    public string SchemaPath { get; }
+    public string Namespace { get; }
+
+    public GeneratedAttributeAttribute(string schemaPath, string @namespace)
+    {
+        SchemaPath = schemaPath;
+        Namespace = @namespace;
+    }
+    
+}
+";
+                ctx.AddSource("GeneratedAttributeAttribute", SourceText.From(sourceText, Encoding.UTF8));
+            });
+        }
+    }
+
+}
